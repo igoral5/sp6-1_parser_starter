dfc3fc0589bf4499ee3c2cb34649b3b857279f49
Igor Alimov <ilalimov@mts.ru>
2026-01-20 21:31:40 +0300

    Rewrite parser in function style

--- parser.js
+++ parser.js
@@ -9,49 +9,50 @@
  * @property {string} title Заголовк страницы
  */
 
-
 /**
  * Производит парсинг заголовка
  * @returns {MetaType} Информация из заголовка страницы
  */
 function parseHeader() {
-    const meta = {opengraph: {}};
-    const html = document.querySelector('html');
-    meta.language = html.lang;
-    const head = document.querySelector('head');
-    for (const elem of head.children) {
-        switch (elem.tagName) {
-            case 'TITLE':
-                meta.title = elem.textContent.split('—')[0].trim();
-                break;
-            case 'META': {
-                switch (elem.name) {
-                    case 'description':
-                        meta.description = elem.content;
-                        break;
-                    case 'keywords': 
-                        meta.keywords = elem.content.split(',').map(value => value.trim());
-                        break;
-                };
-                switch (elem.getAttribute('property')) {
-                    case 'og:title':
-                        meta.opengraph.title = elem.content.split('—')[0].trim();
-                        break;
-                    case 'og:image':
-                        meta.opengraph.image = elem.content;
-                        break;
-                    case 'og:type':
-                        meta.opengraph.type = elem.content;
-                        break;
-                }
-                break;
-            }
+  const html = document.querySelector("html");
+  const language = html.lang;
+  const head = document.querySelector("head");
+  return Array.from(head.children).reduce(
+    (acc, elem) => {
+      switch (elem.tagName) {
+        case "TITLE":
+          acc.title = elem.textContent.split("—")[0].trim();
+          break;
+        case "META": {
+          switch (elem.name) {
+            case "description":
+              acc.description = elem.content;
+              break;
+            case "keywords":
+              acc.keywords = elem.content
+                .split(",")
+                .map((value) => value.trim());
+              break;
+          }
+          switch (elem.getAttribute("property")) {
+            case "og:title":
+              acc.opengraph.title = elem.content.split("—")[0].trim();
+              break;
+            case "og:image":
+              acc.opengraph.image = elem.content;
+              break;
+            case "og:type":
+              acc.opengraph.type = elem.content;
+              break;
+          }
+          break;
         }
-    };
-    return meta;
+      }
+      return acc;
+    },
+    { opengraph: {}, language },
+  );
 }
-
-
 
 /**
  * Возвращает код валюты в зависимости от её обозначения
@@ -59,16 +60,16 @@ function parseHeader() {
  * @returns {string} Код валюты RUB, USD, EUR
  */
 function getCurrencyCode(curr) {
-    switch (curr) {
-        case '₽':
-            return 'RUB';
-        case '$':
-            return 'USD';
-        case '€':
-            return 'EUR';
-        default:
-            return "UNKNOW";
-    }
+  switch (curr) {
+    case "₽":
+      return "RUB";
+    case "$":
+      return "USD";
+    case "€":
+      return "EUR";
+    default:
+      return "UNKNOW";
+  }
 }
 
 /**
@@ -106,57 +107,77 @@ function getCurrencyCode(curr) {
  * @returns {ProductType}
  */
 function parseProduct() {
-    const product = {images: [], properties: {}, tags: {category: [], discount: [], label: []}};
-    const section = document.querySelector('.product');
-    product.id = section.dataset.id;
-    const name = section.querySelector('h1');
-    product.name = name.textContent.trim();
-    const images = [];
-    for (const image of section.querySelectorAll('.preview nav img')) {
-        images.push({
-            full: image.dataset.src,
-            preview: image.src,
-            alt: image.alt
-        })
-    }
-    const imageGeneral = section.querySelector('.preview figure img');
-    const index = images.findIndex(image => image.full === imageGeneral.src);
-    if (index !== 0) 
-        [images[0], images[index]] = [images[index], images[0]];
-    product.images = images;
-    const likeButton = section.querySelector('.preview figure button');
-    product.isLiked = likeButton.classList.contains('active');
-    for (const tag of section.querySelectorAll('.tags span')) {
-        const tagText = tag.textContent.trim();
-        switch (tag.className) {
-            case 'green': 
-                product.tags.category.push(tagText);
-                break;
-            case 'blue':
-                product.tags.label.push(tagText);
-                break;
-            case 'red':
-                product.tags.discount.push(tagText);
-                break;
-        }
-    };
-    const price = section.querySelector('.price');
-    const matchPrice = price.innerHTML.match(/([^\s])(\d+\.?\d*)\s*<span>\s*[^\s](\d+\.?\d*)\s*<\/span>/);
-    if (matchPrice) {
-        product.price = +matchPrice[2];
-        product.oldPrice = +matchPrice[3];
-        product.currency = getCurrencyCode(matchPrice[1]);
-        product.discount = product.oldPrice - product.price;
-        product.discountPercent = `${((product.discount / product.oldPrice) * 100).toFixed(2)}%`;
-    }
-    for (const props of section.querySelectorAll('.properties li')) {
-        const key = props.firstElementChild.textContent.trim();
-        const value = props.lastElementChild.textContent.trim();
-        product.properties[key] = value;
-    }
-    const description = section.querySelector('.description');
-    product.description = description.innerHTML.trim().replace(/<(\w+)([^>]*)>/g, '<$1>');
-    return product;
+  const section = document.querySelector(".product");
+  const id = section.dataset.id;
+  const nameElem = section.querySelector("h1");
+  const name = nameElem.textContent.trim();
+  const images = Array.from(section.querySelectorAll(".preview nav img")).map(
+    (image) => {
+      return {
+        full: image.dataset.src,
+        preview: image.src,
+        alt: image.alt,
+      };
+    },
+  );
+  const imageGeneral = section.querySelector(".preview figure img");
+  const index = images.findIndex((image) => image.full === imageGeneral.src);
+  if (index !== 0) [images[0], images[index]] = [images[index], images[0]];
+  const likeButton = section.querySelector(".preview figure button");
+  const isLiked = likeButton.classList.contains("active");
+  const tags = Array.from(section.querySelectorAll(".tags span")).reduce(
+    (acc, tag) => {
+      const tagText = tag.textContent.trim();
+      switch (tag.className) {
+        case "green":
+          acc.category.push(tagText);
+          break;
+        case "blue":
+          acc.label.push(tagText);
+          break;
+        case "red":
+          acc.discount.push(tagText);
+          break;
+      }
+      return acc;
+    },
+    { category: [], discount: [], label: [] },
+  );
+  const priceElem = section.querySelector(".price");
+  const matchPrice = priceElem.innerHTML.match(
+    /([^\s])(\d+\.?\d*)\s*<span>\s*[^\s](\d+\.?\d*)\s*<\/span>/,
+  );
+  const price = matchPrice ? +matchPrice[2] : undefined;
+  const oldPrice = matchPrice ? +matchPrice[3] : undefined;
+  const currency = matchPrice ? getCurrencyCode(matchPrice[1]) : undefined;
+  const discount = oldPrice - price;
+  const discountPercent = `${((discount / oldPrice) * 100).toFixed(2)}%`;
+  const properties = Array.from(
+    section.querySelectorAll(".properties li"),
+  ).reduce((acc, props) => {
+    const key = props.firstElementChild.textContent.trim();
+    const value = props.lastElementChild.textContent.trim();
+    acc[key] = value;
+    return acc;
+  }, {});
+  const descriptionElem = section.querySelector(".description");
+  const description = descriptionElem.innerHTML
+    .trim()
+    .replace(/<(\w+)([^>]*)>/g, "<$1>");
+  return {
+    id,
+    name,
+    isLiked,
+    tags,
+    price,
+    oldPrice,
+    discount,
+    discountPercent,
+    currency,
+    properties,
+    description,
+    images,
+  };
 }
 
 /**
@@ -173,32 +194,30 @@ function parseProduct() {
  * @returns {OfferType[]}
  */
 function parseSuggested() {
-    const suggested = [];
-    for (const article of document.querySelectorAll('.suggested article')) {
-        const value = {};
-        for(const elem of article.children) {
-            switch (elem.tagName) {
-                case 'IMG':
-                    value.image = elem.src;
-                    break;
-                case 'H3':
-                    value.name = elem.textContent.trim();
-                    break;
-                case 'B':
-                    const matchPrice = elem.textContent.match(/([^\s])(\d+\.?\d*)/);
-                    if (matchPrice) {
-                        value.price = matchPrice[2];
-                        value.currency = getCurrencyCode(matchPrice[1]);
-                    }
-                    break;
-                case 'P': 
-                    value.description = elem.textContent.trim();
-                    break;
+  return Array.from(document.querySelectorAll(".suggested article")).map(
+    (article) =>
+      Array.from(article.children).reduce((acc, elem) => {
+        switch (elem.tagName) {
+          case "IMG":
+            acc.image = elem.src;
+            break;
+          case "H3":
+            acc.name = elem.textContent.trim();
+            break;
+          case "B":
+            const matchPrice = elem.textContent.match(/([^\s])(\d+\.?\d*)/);
+            if (matchPrice) {
+              acc.price = matchPrice[2];
+              acc.currency = getCurrencyCode(matchPrice[1]);
             }
+            break;
+          case "P":
+            acc.description = elem.textContent.trim();
+            break;
         }
-        suggested.push(value);
-    }
-    return suggested;
+        return acc;
+      }, {}),
+  );
 }
 
 /**
@@ -221,41 +240,43 @@ function parseSuggested() {
  * @returns {ReviewType[]}
  */
 function parseReviews() {
-    const reviews = [];
-    for (const article of document.querySelectorAll('.reviews article')) {
-        const value = {};
-        let rating = 0;
-        for (const elem of article.querySelectorAll('.rating span')) {
-            if (elem.className === 'filled')
-                rating += 1;
-            else
-                break;
+  return Array.from(document.querySelectorAll(".reviews article")).map(
+    (article) => {
+      const rating = Array.from(
+        article.querySelectorAll(".rating span"),
+      ).reduce(
+        (acc, elem) => (elem.classList.contains("filled") ? acc + 1 : acc),
+        0,
+      );
+      const authorElem = article.querySelector(".author");
+      const author = Array.from(authorElem.children).reduce((acc, elem) => {
+        switch (elem.tagName) {
+          case "IMG":
+            acc.avatar = elem.src;
+            break;
+          case "SPAN":
+            acc.name = elem.textContent.trim();
+            break;
         }
-        value.rating = rating;
-        const author = {};
-        const authorElem = article.querySelector('.author');
-        for (const elem of authorElem.children) {
-            switch (elem.tagName) {
-                case 'IMG':
-                    author.avatar = elem.src;
-                    break;
-                case 'SPAN':
-                    author.name = elem.textContent.trim();
-                    break;
-                case 'I':
-                    const matchDate = elem.textContent.match(/(\d{2})\/(\d{2})\/(\d{4})/);
-                    if (matchDate)
-                        value.date = `${matchDate[1]}.${matchDate[2]}.${matchDate[3]}`;
-                    break;
-            }
-        }
-        value.author = author;
-        const titleElem = article.querySelector('.title');
-        value.title = titleElem.textContent.trim();
-        value.description = titleElem.nextElementSibling.textContent.trim();
-        reviews.push(value);
-    }
-    return reviews;
+        return acc;
+      }, {});
+      const dateElem = authorElem.querySelector("i");
+      const matchDate = dateElem.textContent.match(/(\d{2})\/(\d{2})\/(\d{4})/);
+      const date = matchDate
+        ? `${matchDate[1]}.${matchDate[2]}.${matchDate[3]}`
+        : undefined;
+      const titleElem = article.querySelector(".title");
+      const title = titleElem.textContent.trim();
+      const description = titleElem.nextElementSibling.textContent.trim();
+      return {
+        title,
+        description,
+        date,
+        author,
+        rating,
+      };
+    },
+  );
 }
 
 /**
@@ -271,12 +292,12 @@ function parseReviews() {
  * @returns {ParseType}
  */
 function parsePage() {
-    return {
-        meta: parseHeader(),
-        product: parseProduct(),
-        suggested: parseSuggested(),
-        reviews: parseReviews(),
-    };
+  return {
+    meta: parseHeader(),
+    product: parseProduct(),
+    suggested: parseSuggested(),
+    reviews: parseReviews(),
+  };
 }
 
 window.parsePage = parsePage;
